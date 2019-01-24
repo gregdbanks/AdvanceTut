063addd6dc0dde9e580531bc50fc255974e8830e
gregdbanks <31157071+gregdbanks@users.noreply.github.com>
2019-01-24 02:02:22 -0600

    map method

---
+++ mapIt/app.js
@@ -0,0 +1,62 @@
+// var arr = [1,2,3];
+
+// var x = arr.map(function(value, index, array) {
+//     return value * 6;
+// });
+
+// console.log(x);
+
+
+// map method DIY
+
+// function map1(arr, callback) {
+//     newArr = [];
+//     for(var i = 0; i < arr; i++) {
+//         newArr.push(callback(arr[i], i, arr));
+        
+//     }
+//     return newArr;
+// }
+
+
+// using map in a function!-------------
+
+// function tripleValues(arr) {
+//     return arr.map(function(value) {
+//         return value * 3;
+//     });
+// }
+
+// console.log(tripleValues([3,2,1]));
+
+// function onlyFirstName(arr) {
+//     return arr.map(function (value) {
+//         return value.first;
+//     });
+// }
+
+// console.log(onlyFirstName([{first: "Greg", last: "smith"}, {first: "Sam", last: "adams"}]));
+
+function doubleValues(arr) {
+    return arr.map(function(val, index) {
+        return val * index;
+    });
+}
+
+console.log(doubleValues([4,5,6]));
+
+function extractKey(arr, key) {
+    return arr.map(function (val) {
+        return val.name;
+    });
+}
+
+console.log(extractKey([{ name: 'Elie' }, { name: 'Tim' }, { name: 'Matt' }, { name: 'Colt' }], name));
+
+function extractFullName(arr) {
+    return arr.map(function (val) {
+        return val.first + " " + val.last;
+    });
+}
+
+console.log(extractFullName([{ first: 'Elie', last: "Schoppik" }, { first: 'Tim', last: "Garcia" }, { first: 'Matt', last: "Lane" }, { first: 'Colt', last: "Steele" }]))
