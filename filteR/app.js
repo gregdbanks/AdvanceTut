47c70cc0cb2c5f5ecea6f0dab744c3e51d9f1df6
gregdbanks <31157071+gregdbanks@users.noreply.github.com>
2019-01-26 02:44:41 -0600

    finishing filter exercise

--- filteR/app.js
+++ filteR/app.js
@@ -85,17 +85,46 @@
 
 
 
-function find(arr, searchValue) {
-    return arr.filter(function(val){
-        return val === searchValue;
-    })[0];
+// function find(arr, searchValue) {
+//     return arr.filter(function(val){
+//         return val === searchValue;
+//     })[0];
+// }
+
+// var x = find([{ first: 'Elie', last: "Schoppik" },
+//                 { first: 'Tim', last: "Garcia", isCatOwner: true },
+//                 { first: 'Matt', last: "Lane" },
+//                 { first: 'Colt', last: "Steele", isCatOwner: true }],
+//                     'isCatOwner', true);
+// console.log(x);
+
+/*
+Write a function called findInObj which accepts an array of objects, a key, and some value to search for and returns the first found value in the arrayt.
+
+Examples:
+    findInObj([{first: 'Elie', last:"Schoppik"}, {first: 'Tim', last:"Garcia", isCatOwner: true}, {first: 'Matt', last:"Lane"}, {first: 'Colt', last:"Steele", isCatOwner: true}], 'isCatOwner',true) // {first: 'Tim', last:"Garcia", isCatOwner: true}
+*/
+// function findInObj(arr, key, searchValue) {
+//     return arr.filter(function(val) {
+//         return val[key] === searchValue;
+//     })[0];
+// }
+
+// var x = findInObj([{ first: 'Elie', last: "Schoppik" },
+//                 { first: 'Tim', last: "Garcia", isCatOwner: true },
+//                 { first: 'Matt', last: "Lane" },
+//                 { first: 'Colt', last: "Steele", isCatOwner: true }],
+//                     'isCatOwner', true);
+
+// console.log(x);
+
+function removeVowels(str) {
+    var vowels = 'aeiou';
+    return str.toLowerCase().split("").filter(function(val) {
+        return vowels.indexOf(val) === -1
+    }).join('');
 }
 
-var x = find([{ first: 'Elie', last: "Schoppik" },
-                { first: 'Tim', last: "Garcia", isCatOwner: true },
-                { first: 'Matt', last: "Lane" },
-                { first: 'Colt', last: "Steele", isCatOwner: true }],
-                    'isCatOwner', true);
+var x = removeVowels("GregLikesLongWlaksOnBeach");
+
 console.log(x);
-
-
