907b1dfe1af4756e67d42c30ac5f249072942f8d
gregdbanks <31157071+gregdbanks@users.noreply.github.com>
2019-04-12 04:27:22 -0500

    finished barChart

--- D3/olympics/app.js
+++ D3/olympics/app.js
@@ -22,11 +22,13 @@ var y = d3.scaleLinear()
 // append the svg object to the body of the page
 // append a 'group' element to 'svg'
 // moves the 'group' element to the top left margin
-var svg = d3.select("body").append("svg")
-                            .attr("width", width + margin.left + margin.right)
-                            .attr("height", height + margin.top + margin.bottom)
-                            .append("g")
-                            .attr("transform",
+var svg = d3.select("body")
+                .append("svg")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+                .style("background-image", "url(img/back.png)")
+                .append("g")
+                .attr("transform",
                                 "translate(" + margin.left + "," + margin.top + ")");
 
 
@@ -101,7 +103,7 @@ d3.csv("test.csv", (error, data) => {
     svg.append("g")
         .call(d3.axisLeft(y));
 
-    // add title
+
     
 
 });
