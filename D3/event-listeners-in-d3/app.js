781958695da52f454e3c1097fe8c238780e8d386
gregdbanks <gregdbanks@yahoo.com>
2019-04-22 03:48:09 -0500

    Some DOM stuff again

--- D3/event-listeners-in-d3/app.js
+++ D3/event-listeners-in-d3/app.js
@@ -1,17 +1,45 @@
-// d3.select("#new-note").on("submit", function(){
+
+d3.select(".remove")
+    .on('click', function() {
+      d3.selectAll('.note')
+        .remove()
+    });
+
+d3.select(".lucky")
+    .on('click', function() {
+      d3.selectAll('.note')
+        .style('font-size', function() {
+          return Math.random() * 100 + 'px';
+        });
+    });
+
+var input = d3.select('input');
+var preview = d3.select('.preview');
+
+d3.select("#new-note")
+    .on("submit", function(){
     
-//     d3.event.preventDefault();
+        d3.event.preventDefault();
 
-//     var input = d3.select('input');
+        var input = d3.select('input');
 
-//     d3.select('#notes')
-//       .append('p')
-//         .classed('note', true)
-//         .text(input.property('value'));
-//     input.property("value", "")
-// });
+        d3.select('#notes')
+          .append('p')
+            .classed('note', true)
+            .text(input.property('value'));
+        input.property("value", "")
+        setPreview("");
+    });
 
-// d3.selectAll('p').remove();
+input.on('input', function() {
+  var note = d3.event.target.value;
+  setPreview(note);
+});
+
+function setPreview(val) {
+  preview.text(val)
+          .classed('hide', val === "");
+}
 
 
 // map:
@@ -42,10 +70,10 @@
 
 // console.log(total);
 
-// Advance reduce():
+// Advance reduce()
 
-import fs from 'fs'
+// import fs from 'fs'
 
-var output = fs.readFileSync('data.txt', 'utf8');
+// var output = fs.readFileSync('data.txt', 'utf8');
 
-console.log('output', output);
+// console.log('output', output);
