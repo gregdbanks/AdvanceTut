0525e3f3f38360fa5be4d61521ab92f033cca16c
gregdbanks <gregdbanks@yahoo.com>
2019-04-24 04:03:34 -0500

    Starting Scott Murray book

--- D3/extrema_and_scales/app.js
+++ D3/extrema_and_scales/app.js
@@ -1,70 +1,107 @@
-
-
 //  get the data prepared for your scale and input type=range
 
 // these are for input type=range
-let minYear   = d3.min(birthData, d => d.year),
-    maxYear   = d3.max(birthData, d => d.year),
+let minYear = d3.min(birthData, d => d.year),
+  maxYear = d3.max(birthData, d => d.year),
 
-// this is for yscale rects
-    maxBirths = d3.max(birthData, d => d.births);
+
+
+  // this is for yscale rects
+maxBirths = d3.max(birthData, d => d.births);
+
+
+
+
+
+// months for xAxis
+let grabMonths = birthData.map(d => d.month);
+
+// filter duplicates
+function removeDuplicateUsingFilter(arr) {
+  let unique_array = arr.filter(function (elem, index, self) {
+    return index == self.indexOf(elem);
+  });
+  return unique_array
+}
+
+let months = removeDuplicateUsingFilter(grabMonths);
+
+
 
 
 
 // set svg demensions
-
-let width      = 600,
-    height     = 600,
-    barPadding = 10,
-    numBars    = 12,
-    barWidth   = width / numBars - barPadding
+let width = 800,
+  height = 400,
+  barPadding = 10,
+  numBars = 12,
+  barWidth = width / numBars - barPadding 
 
 // make scale matching 0 to max data, and height to 0
 //    0-----------------------------2255345
 //    \                                \
 //  600--------------------------------0
 
-let yScale = 
+let yScale =
   d3.scaleLinear()
-    .domain([0, maxBirths])
-    .range([height, 0])
+  .domain([0, maxBirths])
+  .range([height, 0])
+
+
+
+// adding yAxis
+let y_axis = d3.axisLeft()
+  .scale(yScale);
+
+
+let xScale =
+  d3.scaleLinear()
+  .domain(d3.extent(birthData, d => d.months));
+
+
+let x_axis = d3.axisBottom()
+  .scale(xScale)
+  .ticks(12);
 
 
 //  make an input type=range
 
 
-  d3.select("input")
-    .property("min"  , minYear)
-    .property("max"  , maxYear)
-    .property("value", maxYear);
+d3.select("input")
+  .property("min", minYear)
+  .property("max", maxYear)
+  .property("value", maxYear);
 
 
 // create svg and append rect
 
 
 d3.select("svg")
-    .attr("width" , width)
-    .attr("height", height)
-    .style('background', '#F8F9FA')
+  .attr("width", width)
+  .attr("height", height)
+  .style('background', '#F8F9FA')
   .selectAll("rect")
-  .data( birthData.filter(d => d.year === maxYear ) )
+  .data(birthData.filter(d => d.year === maxYear))
   .enter()
   .append("rect")
-    .attr("fill"  , "#355DCE") 
-    .attr("width" , barWidth) 
-    .attr("x"     , (d, i) => (barWidth + barPadding) * i )
-    .attr("height", d => height - yScale(d.births)) 
-    .attr("y"     , d => yScale(d.births) ) //can remove if you want bars coming from top
-    .style({"border-radius":"0, 0, 10px, 10px"});
-    
+  .attr("fill", "#355DCE")
+  .attr("width", barWidth)
+  .attr("x", (d, i) => (barWidth + barPadding) * i)
+  .attr("height", d => height - yScale(d.births))
+  .attr("y", d => yScale(d.births)) //can remove if you want bars coming from top
+  .style('float', 'right')
+
+
+// appending yAxis
+d3.select("svg")
+  .append("g")
+  .attr("transform", "translate(60, 10)")
+  .call(y_axis);
+
+// appending xAxis
+d3.select("svg")
+  .append("g")
+  .attr("transform", "translate(0," + height + ")")
+  .call(x_axis);
+
 // setup event listener 
-
-
-d3.select("input")
-    .on("input", () => {
-      let year = +d3.event.target.value;
-      d3.selectAll("rect")
-        .data(birthData.filter( d => d.year === year ))
-          .attr("height", d => height - yScale(d.births) )
-          .attr("y"     , d => yScale(d.births));
-    });
